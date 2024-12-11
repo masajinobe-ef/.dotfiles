ba16e8e59532ace548a983610e80f76aac0e8041
masajinobe-ef <priscilla.effects@gmail.com>
2024-12-11 10:23:53 +0500

    up

---
+++ i3/scripts/qemu.sh
@@ -0,0 +1,157 @@
+#!/usr/bin/env bash
+
+vm_name="linux_vm"
+iso_path="./linux.iso"
+disk_image="./linux_vm.img"
+disk_size="20g"
+memory="2048"
+cpus="2"
+distros=("ubuntu" "debian" "fedora" "arch")
+
+required_packages=("qemu-full" "curl")
+
+check_and_install_packages() {
+    for pkg in "${required_packages[@]}"; do
+        if ! pacman -qq "$pkg" &>/dev/null; then
+            echo "Installing $pkg..."
+            sudo pacman -s --noconfirm "$pkg"
+            if [ $? -ne 0 ]; then
+                echo "Error: failed to install package $pkg. Make sure you have internet access and root privileges."
+                exit 1
+            fi
+        fi
+    done
+}
+
+check_qemu_installed() {
+    if ! command -v qemu-system-x86_64 &>/dev/null; then
+        echo "Error: qemu is not installed. Install it via 'sudo pacman -s qemu'."
+        exit 1
+    fi
+}
+
+show_help() {
+    cat <<eof
+Usage: $0 [options]
+
+Options:
+  --name <name>         Set the name of the virtual machine (default: linux_vm)
+  --iso <path>          Specify the path to the ISO image (default: ./linux.iso)
+  --disk <path>         Specify the path for the virtual disk (default: ./linux_vm.img)
+  --size <size>         Specify the size of the virtual disk (default: 20g)
+  --memory <MB>         Specify the amount of memory in MB (default: 2048)
+  --cpus <count>        Specify the number of CPUs (default: 2)
+  --download <distro>   Download the ISO image of a popular Linux distro (${distros[*]})
+  --help                Show this message and exit
+eof
+}
+
+download_iso() {
+    local distro=$1
+    local url=""
+    case $distro in
+    ubuntu)
+        url="https://releases.ubuntu.com/24.04.1/ubuntu-24.04.1-desktop-amd64.iso"
+        ;;
+    debian)
+        url="https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.8.0-amd64-netinst.iso"
+        ;;
+    fedora)
+        url="https://mirror.nl.mirhosting.net/fedora/linux/releases/41/server/x86_64/iso/fedora-server-dvd-x86_64-41-1.4.iso"
+        ;;
+    arch)
+        url="https://archlinux.gay/archlinux/iso/2024.12.01/archlinux-2024.12.01-x86_64.iso"
+        ;;
+    *)
+        echo "Error: unsupported distro ($distro). Available: ${distros[*]}"
+        exit 1
+        ;;
+    esac
+
+    echo "Downloading $distro from $url..."
+    curl -o "$iso_path" "$url"
+    if [ $? -eq 0 ]; then
+        echo "$distro image successfully downloaded to $iso_path"
+    else
+        echo "Error downloading $distro image"
+        exit 1
+    fi
+}
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    --name)
+        vm_name="$2"
+        shift 2
+        ;;
+    --iso)
+        iso_path="$2"
+        shift 2
+        ;;
+    --disk)
+        disk_image="$2"
+        shift 2
+        ;;
+    --size)
+        disk_size="$2"
+        shift 2
+        ;;
+    --memory)
+        memory="$2"
+        shift 2
+        ;;
+    --cpus)
+        cpus="$2"
+        shift 2
+        ;;
+    --download)
+        download_iso "$2"
+        exit 0
+        ;;
+    --help)
+        show_help
+        exit 0
+        ;;
+    *)
+        echo "Unknown parameter: $1"
+        show_help
+        exit 1
+        ;;
+    esac
+done
+
+check_and_install_packages
+
+check_qemu_installed
+
+if [ ! -f "$iso_path" ]; then
+    echo "Error: ISO file not found ($iso_path)"
+    exit 1
+fi
+
+if [ ! -f "$disk_image" ]; then
+    echo "Creating virtual disk of size $disk_size..."
+    qemu-img create -f qcow2 "$disk_image" "$disk_size"
+    if [ $? -ne 0 ]; then
+        echo "Error creating virtual disk."
+        exit 1
+    fi
+fi
+
+qemu-system-x86_64 \
+    -name "$vm_name" \
+    -m "$memory" \
+    -smp cpus="$cpus" \
+    -hda "$disk_image" \
+    -cdrom "$iso_path" \
+    -boot d \
+    -enable-kvm \
+    -display default \
+    -net nic -net user
+
+if [ $? -ne 0 ]; then
+    echo "Error launching the virtual machine."
+    exit 1
+fi
+
+echo "Virtual machine $vm_name is running."
---
+++ sym.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+DOTFILES_DIR="$HOME/.dotfiles"
+I3_DIR="$DOTFILES_DIR/i3"
+
+AIR_DIR="$DOTFILES_DIR/air"
+AIR_CONFIG_DIR="$AIR_DIR/config"
+
+BIN_DIR="$I3_DIR/bin"
+SCRIPTS_DIR="$I3_DIR/scripts"
+CONFIG_DIR="$I3_DIR/config"
+HOME_DIR="$I3_DIR/home"
+
+TARGET_BIN="$HOME/.local/bin"
+TARGET_SCRIPTS="$HOME/.local/scripts"
+TARGET_CONFIG="$HOME/.config"
+TARGET_HOME="$HOME"
+
+exit_with_error() {
+    echo -e "${RED}Error: $1${NC}"
+    [ -n "$2" ] && echo "Details: $2"
+    exit 1
+}
+
+delink_directory() {
+    local dir="$1"
+    local target="$2"
+    if [ -d "$dir" ]; then
+        echo -e "${BLUE}Removing symlinks from $target...${NC}"
+        if ! stow -D --dir="$I3_DIR" --target="$target" "$(basename "$dir")"; then
+            exit_with_error "Error removing symlinks for $dir" "$(stow -D --dir="$I3_DIR" --target="$target" "$(basename "$dir")" 2>&1)"
+        fi
+    else
+        exit_with_error "Directory $dir not found."
+    fi
+}
+
+stow_directory() {
+    local dir="$1"
+    local target="$2"
+    [ ! -d "$dir" ] && exit_with_error "Directory $dir is missing."
+
+    if [ ! -d "$target" ]; then
+        echo -e "${BLUE}Creating directory $target...${NC}"
+        if ! mkdir -p "$target"; then
+            exit_with_error "Failed to create directory $target. Check permissions."
+        fi
+    else
+        echo -e "${GREEN}Directory $target already exists.${NC}"
+    fi
+
+    echo -e "${BLUE}Creating symlinks from $dir to $target...${NC}"
+    if ! stow --dir="$I3_DIR" --target="$target" "$(basename "$dir")"; then
+        exit_with_error "Failed to create symlinks from $dir to $target" "$(stow --dir="$I3_DIR" --target="$target" "$(basename "$dir")" 2>&1)"
+    fi
+}
+
+delink_mode=false
+use_air=false
+
+for arg in "$@"; do
+    case $arg in
+    -d)
+        delink_mode=true
+        ;;
+    -m)
+        use_air=true
+        ;;
+    -h)
+        echo "Usage: $(basename "$0") [-d] [-m] [-h]"
+        echo "  -d   Remove existing symlinks."
+        echo "  -m   Use the AIR directory instead of I3."
+        echo "  -h   Show this message."
+        exit 0
+        ;;
+    *)
+        exit_with_error "Unknown argument: $arg"
+        ;;
+    esac
+done
+
+if [ "$delink_mode" = true ]; then
+    delink_directory "$BIN_DIR" "$TARGET_BIN"
+    delink_directory "$SCRIPTS_DIR" "$TARGET_SCRIPTS"
+    delink_directory "$CONFIG_DIR" "$TARGET_CONFIG"
+    delink_directory "$HOME_DIR" "$TARGET_HOME"
+    echo -e "${GREEN}All symlinks successfully removed.${NC}"
+else
+    if [ "$use_air" = true ]; then
+        CONFIG_DIR="$AIR_CONFIG_DIR"
+    fi
+    stow_directory "$BIN_DIR" "$TARGET_BIN"
+    stow_directory "$SCRIPTS_DIR" "$TARGET_SCRIPTS"
+    stow_directory "$CONFIG_DIR" "$TARGET_CONFIG"
+    stow_directory "$HOME_DIR" "$TARGET_HOME"
+    echo -e "${GREEN}All directories successfully symlinked.${NC}"
+fi
+
+read -p "Press Enter to exit..."
